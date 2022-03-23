d08baffcfcd9b99b982e33ddbfa1bf005ad32b58
Bruno Virginio <bruno.virginio23@gmail.com>
2022-03-23 10:31:11 -0300

    First commit

---
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using StockAnalyzer.Core;
+using StockAnalyzer.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace StockAnalyzer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        private static readonly string API_URL = "https://ps-async.fekberg.com/api/stocks";
+        private readonly Stopwatch stopwatch = new Stopwatch();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            BeforeLoadingStockData();
+
+            //using (var httpClient = new HttpClient())
+            //{
+            //    var response = await httpClient.GetAsync($"{API_URL}/{StockIdentifier.Text}");
+            //    var content = await response.Content.ReadAsStringAsync();
+
+            //    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+            //    Stocks.ItemsSource = data;
+            //}
+
+            try
+            {
+                var dataStore = new DataStore();
+
+                var data = await dataStore.GetStockPrices(StockIdentifier.Text);
+                Stocks.ItemsSource = data;
+
+                AfterLoadingStockData();
+            }
+            catch (Exception ex)
+            {
+                Notes.Text = ex.Message;
+            }
+        }
+
+        private void BeforeLoadingStockData()
+        {
+            stopwatch.Restart();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+        }
+
+        private void AfterLoadingStockData()
+        {
+            StocksStatus.Text = $"Loaded stocks for {StockIdentifier.Text} in {stopwatch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
